92e516be371a8992707970caa7c31215d83b2527
Santii08 <dan.santiago.rc2005@gmail.com>
2024-05-05 17:52:39 -0500

    1.0.0

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const PORT = 8080;
 
 app.use(express.json());
+app.use(cors());
+
+// Corrección en las rutas de los módulos
+app.use("/api/signup", require("./routes/signup"));
+app.use("/api/login", require("./routes/login"));
+app.use("/api/refreshToken", require("./routes/refreshToken"));
+app.use("/api/logout", require("./routes/logout"));
+app.use("/api/todos", require("./routes/todos"));
+app.use("/api/user", require("./routes/user"));
 
 app.get("/inicio", (req, res) => {
   const name = "Daniel Santiago";
   res.json(name);
 });
+
 app.listen(PORT, () =>
-  console.log(`This api in running on http://localhost:${PORT}`)
+  console.log(`Esta API está corriendo en http://localhost:${PORT}`)
 );
