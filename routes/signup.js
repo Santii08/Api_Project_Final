b5bf685341c75c9d8d73772d5e1949c114179f5a
Santii08 <dan.santiago.rc2005@gmail.com>
2024-05-05 20:44:35 -0500

    problema deploy solucionado

--- routes/signup.js
+++ routes/signup.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { jsonResponse } = require("../lib/jsonresponse");
+const { jsonResponse } = require("../lib/jsonResponse");
 const router = express.Router();
 
 // Middleware para la ruta '/api/signup'
 router.post("/", (req, res) => {
-  const { name, birthday, mail, username, password } = req.body;
-  if (!!!name || !!!birthday || !!!mail || !!!username || !!!password) {
+  const { name, birthday, email, username, password } = req.body;
+  if (!name || !birthday || !email || !username || !password) {
     return res.status(400).json(
       jsonResponse(400, {
         error: "Necesitas completar todo",
@@ -15,7 +15,6 @@ router.post("/", (req, res) => {
   res
     .status(200)
     .json(jsonResponse(200, { message: "Usario creado correctamente" }));
-  res.send("signup");
 });
 
 module.exports = router;
